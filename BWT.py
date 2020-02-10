2374489043a62c81a9d765a1c2d5882f1779f03b
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-02-09 20:09:38 -0500

    Add files via upload

---
+++ BWT.py
@@ -0,0 +1,90 @@
+# BWT.py
+# HW1, Computational Genomics, Spring 2020
+# andrewid: slevineg
+
+# WARNING: Do not change the file name, or the function signatures below.
+# Autograder expects these names exactly.
+
+def rle(s):
+    """Run Length Encoder
+    Args: s, string to be encoded
+    Returns: RLE(s)
+    """
+
+    def countRL(str, k):
+        currcount = 0
+        for char in str[k:]:
+            if str[k] == char:
+                currcount += 1
+            else:
+                break  # stop scanning to save effort
+
+        return currcount
+
+    for i in range(0, len(s)-1):
+
+        if countRL(s, i) >= 2:
+
+            rl = countRL(s, i)  # count of consecutive characters at s[i]
+            #now to remove characters
+
+            s = s[:i+2]+str(rl)+s[i+rl:]
+
+    return s
+
+
+def bwt_encode(s):
+    """Burrows-Wheeler Transform
+    Args: s, string, which must not contain '{' or '}'
+    Returns: BWT(s), which contains '{' and '}'
+    """
+    s = "{" + s + "}"  # adding marker to start/end of text
+
+    # generate an array of circularly-rotated strings, sorted,
+    table = sorted(s[i:] + s[:i] for i in range(len(s)))
+
+    #get last characters from each row.
+    last_column = [row[-1:] for row in table]
+
+    # get string of last characters in each column.
+    return "".join(last_column)
+
+
+def bwt_decode(bwt):
+    """Inverse Burrows-Wheeler Transform
+    Args: bwt, BWT'ed string, which should contain '{' and '}'
+    Returns: reconstructed original string s, must not contains '{' or '}'
+    """
+    table = [""] * len(bwt)  # make an empty table
+ 
+    for i in range(len(bwt)):
+        table = sorted(bwt[i] + table[i]
+                       for i in range(len(bwt)))  # Add a column of r
+    # Find the correct row (ending in '}')
+
+    s = [row for row in table if row.endswith("}")][0]
+
+    return s.rstrip("}").strip("{")  # Get rid of start and end markers
+
+
+def test_string(s):
+    compressed = rle(s)
+    bwt = bwt_encode(s)
+    compressed_bwt = rle(bwt)
+    reconstructed = bwt_decode(bwt)
+    template = "{:25} ({:3d}) {}"
+    print(template.format("original", len(s), s))
+    print(template.format("bwt_enc(orig)", len(bwt), bwt))
+    print(template.format("bwt_dec(bwt_enc(orig))", len(reconstructed), reconstructed))
+    print(template.format("rle(orig)", len(compressed), compressed))
+    print(template.format("rle(bwt_enc(orig))", len(compressed_bwt), compressed_bwt))
+    print()
+    print()
+
+
+if __name__ == "__main__":
+    # Add more of your own strings to explore for question (i)
+    test_strings = ["WOOOOOHOOOOHOOOO!",
+                    "scottytartanscottytartanscottytartanscottytartan"]
+    for s in test_strings:
+        test_string(s)
---
+++ NW.py
@@ -0,0 +1,138 @@
+# NW.py
+# HW1, Computational Genomics, Spring 2020
+# andrewid: slevineg
+
+# WARNING: Do not change the file name; Autograder expects it.
+import sys
+import numpy as np
+
+#python NW.py example.fasta to run
+
+def ReadFASTA(filename):
+    fp=open(filename, 'r')
+    Sequences={}
+    tmpname=""
+    tmpseq=""
+    for line in fp:
+        if line[0]==">":
+            if len(tmpseq)!=0:
+                Sequences[tmpname]=tmpseq
+            tmpname=line.strip().split()[0][1:]
+            tmpseq=""
+        else:
+            tmpseq+=line.strip()
+    Sequences[tmpname]=tmpseq
+    fp.close()
+    return Sequences
+
+# You may define any helper functions for Needleman-Wunsch algorithm here
+
+
+# def Score(char1,char2): 
+#     if char1 == char2:
+#         return 0 #match
+    
+#     return -2 #mismatch
+
+def score_chars(char1,char2): 
+    if char1 == char2:
+        return 0  # match
+    elif char1 == '-' or char2 == '-':
+        return -1 #gap w/ one character
+    else:
+        return -2 #mismatch
+
+
+def F_matrix(seq1,seq2):
+    #mismatch = -2, gap = -1, match = 0
+    F=np.empty([len(seq1)+1,len(seq2)+1])
+    
+    d = -1 #gap cost
+    for i in range(0,len(seq1)+1):
+        F[i][0] = d*i
+    for j in range(0,len(seq2)+1):
+        F[0][j] = d*j
+
+
+    for i in range(1, len(seq1)+1):
+        for j in range(1,len(seq2)+1):
+            match = F[i-1][j-1] + score_chars(seq1[i-1],seq2[j-1])
+             #score: cost of matching characters: 0 if same, -2 if mismatch
+            delete = F[i-1][j] + d #gap cost
+            insert = F[i][j-1] + d
+
+            F[i][j] = max(match,insert,delete)
+    print(F)
+    return F
+
+
+def traceback(F, seq1, seq2):
+    
+    d = -1
+    
+    i = len(seq1)
+    j = len(seq2)
+    Alignment1 = ""
+    Alignment2 = ""
+
+
+    score = 0
+    # score = score_chars(seq1[len(seq1)-1],seq2[len(seq2)-1])
+
+    while (i>0 or j>0): #stay in matrix bounds
+
+        
+
+        if F[i][j] == F[i-1][j-1] + score_chars(seq1[i-1], seq2[j-1]) :
+            #match or mismatch case
+            Alignment1 = seq1[i-1] + Alignment1
+            Alignment2 = seq2[j-1] + Alignment2
+
+            score += score_chars(seq1[i-1], seq2[j-1])
+            # print(i,j)
+            i += -1
+            j += -1
+
+
+        elif ((i > 0) and (F[i][j] == F[i-1][j] + d)):
+            #gap case, seq2 
+            Alignment1 = seq1[i-1] + Alignment1
+            Alignment2 = "-" + Alignment2 #gap character
+            score += -1
+            # print(i,j)
+            i += -1
+
+        else: #gap for seq2
+            Alignment1 = "-" + Alignment1
+            Alignment2 = seq2[j-1] + Alignment2
+            score += -1
+            # print(i,j)
+            j += -1
+    
+    #finally, score the alignment
+    # score = 99
+    return Alignment1, Alignment2, score
+
+
+
+# Do not change this function signature
+def needleman_wunsch(seq1, seq2):
+    """Find the global alignment for seq1 and seq2
+    Returns: a string of three lines like so:
+    '<alignment score>\n<alignment in seq1>\n<alignment in seq2>'
+    """
+    F = F_matrix(seq1,seq2)
+    A1,A2,score = (traceback(F, seq1, seq2))
+
+    # out_str = "strscore}\n{A1}\n{A2}"
+    out_str = str(score)+"\n"+str(A1)+"\n"+str(A2)
+    return out_str
+
+if __name__=="__main__":
+    Sequences=ReadFASTA(sys.argv[1])
+    assert len(Sequences.keys())==2, "fasta file contains more than 2 sequences."
+    seq2=Sequences[list(Sequences.keys())[0]]
+    seq1=Sequences[list(Sequences.keys())[1]]
+    # print(needleman_wunsch('AGCTA', 'AGGTCA'))
+    print(needleman_wunsch(seq1,seq2))
+
